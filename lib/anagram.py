67a5ccacdb879c07c410921d3301d93e6635b0d5
Daniel <daniel.mwangi@student.moringaschool.com>
2023-12-01 21:13:04 +0300

    committed anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        return [candidate for candidate in possible_anagrams if self.is_anagram(candidate)]
+
+    def is_anagram(self, candidate):
+        return sorted(self.word) == sorted(candidate)
